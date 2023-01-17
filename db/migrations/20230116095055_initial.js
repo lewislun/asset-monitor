d8bd9f06bf246060891e6eef85084f3c87f5ecb5
Lewis Cheung <lewislunsj@gmail.com>
2023-01-17 11:43:37 +0800

    feat: asset-history-batch

--- db/migrations/20230116095055_initial.js
+++ db/migrations/20230116095055_initial.js
@@ -1,23 +1,34 @@
 'use strict'
 
-import { AssetHistory } from '../../lib/index.js'
+import { AssetHistory, AssetHistoryBatch } from '../../lib/index.js'
 
 /**
  * @param {import('knex').Knex} knex
  * @returns {Promise<void>}
  */
 export async function up(knex) {
-  await knex.schema.createTable(AssetHistory.tableName, t => {
-	t.increments('id').primary()
-	t.string('code', 255).notNullable().index()
-	t.string('type', 255).notNullable().index()
-	t.string('name', 255).notNullable().index()
-	t.string('state', 255).notNullable().index()
-	t.decimal('quantity', 36, 18).notNullable()
-	t.decimal('usd_value', 15, 6).notNullable()
-	t.decimal('usd_value_per_quantity', 15, 6).notNullable()
-	t.timestamp('captured_at').notNullable()
-  })
+	await knex.schema.createTable(AssetHistoryBatch.tableName, t => {
+		t.increments('id').primary()
+		t.decimal('total_usd_value', 15, 6).notNullable()
+		t.timestamp('captured_at').notNullable()
+	})
+
+	await knex.schema.createTable(AssetHistory.tableName, t => {
+		t.increments('id').primary()
+		t.integer('batch_id').notNullable().unsigned().index()
+		t.string('code', 255).notNullable().index()
+		t.string('type', 255).notNullable().index()
+		t.string('name', 255).notNullable().index()
+		t.string('state', 255).notNullable().index()
+		t.decimal('quantity', 36, 18).notNullable()
+		t.decimal('usd_value', 15, 6).notNullable()
+		t.decimal('usd_value_per_quantity', 15, 6).notNullable()
+		t.timestamp('captured_at').notNullable()
+
+		t.foreign('batch_id')
+			.references('id')
+			.inTable(AssetHistoryBatch.tableName)
+	})
 }
 
 /**
@@ -25,5 +36,6 @@ export async function up(knex) {
  * @returns {Promise<void>}
  */
 export async function down(knex) {
-  await knex.schema.dropTable(AssetHistory.tableName)
+  await knex.schema.dropTableIfExists(AssetHistory.tableName)
+  await knex.schema.dropTableIfExists(AssetHistoryBatch.tableName)
 }
