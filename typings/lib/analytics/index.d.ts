a945826781b9c4eaef640179f47bd895dcbb9b2e
Lewis Cheung <lewislunsj@gmail.com>
2023-02-10 10:34:29 +0800

    fix: typings

--- typings/lib/analytics/index.d.ts
+++ typings/lib/analytics/index.d.ts
@@ -1,12 +1,33 @@
 /**
+ * @typedef TotalValueData
+ * @property {types.AssetCode} [code]
+ * @property {types.Chain} [chain]
+ * @property {enums.AssetType} [type]
+ * @property {enums.AssetState} [state]
+ * @property {string} [tagValue]
+ * @property {Decimal} usdValue
+ *
+ *
  * @param {object} [opts={}]
  * @param {Transaction} [opts.trx]
  * @param {'code'|'chain'|'group'|'tag'|'type'|'state'} [opts.groupBy]
  * @param {string} [opts.tagCategory] this is required of opts.groupBy === 'tag'
+ * @returns {Promise<TotalValueData[]>}
  */
 export function getTotalValue(opts?: {
     trx?: Transaction;
     groupBy?: 'code' | 'chain' | 'group' | 'tag' | 'type' | 'state';
     tagCategory?: string;
-}): Promise<any[]>;
+}): Promise<TotalValueData[]>;
 export type Transaction = import('objection').Transaction;
+export type TotalValueData = {
+    code?: types.AssetCode;
+    chain?: types.Chain;
+    type?: enums.AssetType;
+    state?: enums.AssetState;
+    tagValue?: string;
+    usdValue: Decimal;
+};
+import * as types from "../types.js";
+import * as enums from "../enums.js";
+import Decimal from "decimal.js";
