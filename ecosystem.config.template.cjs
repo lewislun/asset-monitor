f1820b031966f04edd95210f86a958fb8d998273
Lewis Cheung <lewislunsj@gmail.com>
2023-02-10 10:34:34 +0800

    fix: suppress-experimental-warnings

--- ecosystem.config.template.cjs
+++ ecosystem.config.template.cjs
@@ -1,7 +1,7 @@
 module.exports = {
 	apps : [{
 		name: 'asset-monitor',
-		script: 'node ./cli monitor \'0 * * * *\'',
+		script: 'node --require=suppress-experimental-warnings ./cli monitor \'0 * * * *\'',
 		max_memory_restart: '256M',
 		combine_logs: true,
 	}],
