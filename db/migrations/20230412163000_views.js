6eb8a23d0048168d56081cbc970083437c64ec60
Lewis Cheung <lewislunsj@gmail.com>
2023-04-13 17:06:10 +0800

    feat: added 1 day, 7 days and 30 days ago usd value

--- db/migrations/20230412163000_views.js
+++ db/migrations/20230412163000_views.js
@@ -27,18 +27,43 @@ export async function up(knex) {
 	})
 
 	await knex.schema.createMaterializedView(SummaryView.tableName, v => {
+		// Query for current USD value
 		const currentUsdValueQuery = BatchListView.query(knex)
 			.select('usd_value')
 			.orderBy('scan_started_at', 'desc')
 			.limit(1)
 			.toKnexQuery()
-	
-		const lastScannedAtQuery = BatchListView.query(knex)
-			.select('scan_started_at')
+
+		// Query for USD value 1 day ago
+		const oneDayAgoUsdValueQuery = BatchListView.query(knex)
+			.select('usd_value')
 			.orderBy('scan_started_at', 'desc')
+			.where('scan_started_at', '<', knex.raw('now() - interval \'1 day\''))
 			.limit(1)
 			.toKnexQuery()
 
+		// Query for USD value 7 days ago
+		const sevenDayAgoUsdValueQuery = BatchListView.query(knex)
+			.select('usd_value')
+			.orderBy('scan_started_at', 'desc')
+			.where('scan_started_at', '<', knex.raw('now() - interval \'7 day\''))
+			.limit(1)
+			.toKnexQuery()
+
+		// Query for USD value 30 days ago
+		const thirtyAgoUsdValueQuery = BatchListView.query(knex)
+			.select('usd_value')
+			.orderBy('scan_started_at', 'desc')
+			.where('scan_started_at', '<', knex.raw('now() - interval \'30 day\''))
+			.limit(1)
+			.toKnexQuery()
+	
+		// Query for last scanned at
+		const lastScannedAtQuery = BatchListView.query(knex)
+			.max('scan_started_at')
+			.toKnexQuery()
+
+		// Query for total inflow, outflow, and net inflow
 		const totalInflowQuery = AssetFlow.query(knex)
 			.sum('usd_value')
 			.whereNull('from_group_id')
@@ -47,19 +72,33 @@ export async function up(knex) {
 			.select(knex.raw('coalesce(sum(0 - usd_value), 0) as usd_value'))
 			.whereNull('to_group_id')
 			.toKnexQuery()
+		const netInflowQuery = knex.raw(`(${totalInflowQuery}) - (${totalOutflowQuery})`)
 
-		v.columns(['last_scanned_at', 'current_usd_value', 'total_inflow', 'total_outflow', 'net_inflow', 'pnl_percent'])
+		v.columns([
+			'total_inflow',
+			'total_outflow',
+			'net_inflow',
+			'last_scanned_at',
+			'current_usd_value',
+			'one_day_ago_usd_value',
+			'seven_day_ago_usd_value',
+			'thirty_day_ago_usd_value',
+			'pnl_percent',
+		])
 		v.as(knex.raw(`
 			select
 				t.*,
-				round((t.current_usd_value - t.net_inflow) / t.net_inflow * 100, 2) as pnl_percent
+				round((t.current_usd_value - t.net_inflow) / t.net_inflow * 100, 2) as pnl_percent,
 			from (
 				select
-					(${lastScannedAtQuery}) as last_scanned_at,
-					(${currentUsdValueQuery}) as current_usd_value,
 					(${totalInflowQuery}) as total_inflow,
 					(${totalOutflowQuery}) as total_outflow,
-					((${totalInflowQuery}) - (${totalOutflowQuery})) as net_inflow
+					(${netInflowQuery}) as net_inflow,
+					(${lastScannedAtQuery}) as last_scanned_at,
+					(${currentUsdValueQuery}) as current_usd_value,
+					(${oneDayAgoUsdValueQuery}) as one_day_ago_usd_value,
+					(${sevenDayAgoUsdValueQuery}) as seven_day_ago_usd_value,
+					(${thirtyAgoUsdValueQuery}) as thirty_day_ago_usd_value
 			) as t
 		`))
 	})
