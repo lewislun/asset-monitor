57576828e59c0b4826421131bc70c210184ce177
Lewis Cheung <lewislunsj@gmail.com>
2023-04-14 15:59:37 +0800

    feat: cmd to remove specific batch

---
+++ cli/batch.js
@@ -0,0 +1,26 @@
+import { Command } from 'commander'
+
+import * as lib from '../lib/index.js'
+
+const cmd = new Command('batch')
+const logger = lib.createLogger('CLI')
+
+cmd
+	.description('Asset batch management.')
+	.addCommand(new Command('remove')
+		.argument('<id>', 'id of the batch')
+		.action(async id => {
+			logger.info(`Removing an AssetSnapshotBatch with id: ${id}...`)
+			const affectedRowCount = await lib.AssetSnapshotBatch.query().findById(id).delete()
+			if (affectedRowCount === 0) {
+				logger.error(`AssetSnapshotBatch not found - id: ${id}`)
+				process.exit(1)
+			}
+
+			await lib.refreshMaterializedViews()
+			logger.info(`AssetSnapshotBatch deleted - id: ${id}`)
+			process.exit(0)
+		})
+	)
+
+export default cmd
--- cli/index.js
+++ cli/index.js
@@ -3,6 +3,7 @@ import { Model } from 'objection'
 
 import * as lib from '../lib/index.js'
 import { knex } from '../db/index.js'
+import batchCmd from './batch.js'
 import flowCmd from './flow.js'
 import groupCmd from './group.js'
 import monitorCmd from './monitor.js'
@@ -13,7 +14,7 @@ import packageJson from '../package.json' assert { type: 'json' }
 
 Model.knex(knex)
 const program = new Command()
-const cmds = [scanCmd, monitorCmd, flowCmd, groupCmd, userCmd, queryCmd]
+const cmds = [scanCmd, monitorCmd, flowCmd, groupCmd, userCmd, queryCmd, batchCmd]
 
 program
 	.description('Web3 Asset Monitor')
